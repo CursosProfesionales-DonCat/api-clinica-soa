c4aeca8edc730436d2477ca5762583a0c809d087
CursosProfesionales-DonCat <juniormottasoto5@gmail.com>
2026-06-12 15:22:31 -0500

    Update database connection to Supabase

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# ¡AQUÍ ESTÁ LA CONEXIÓN A TU BD LOCAL "soa"!
-SQLALCHEMY_DATABASE_URL = "postgresql://db_clinica_g2_user:zdPzp8MYGqfcUtgvCFZERuCEHLFZkUO3@dpg-d8ho3a5dt1ts73egtpdg-a.oregon-postgres.render.com/db_clinica_g2"
+# ¡NUEVA CONEXIÓN A SUPABASE (El Puente)!
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Dn35vNv8dJeMi2Qv@db.okydwhigvrzdjfzumvun.supabase.co:5432/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
