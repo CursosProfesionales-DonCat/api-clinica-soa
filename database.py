000d66bce59bd371bf74efc4ad626043925864fa
CursosProfesionales-DonCat <juniormottasoto5@gmail.com>
2026-07-06 23:29:00 -0500

    Update database connection URL for Supabase

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # ¡NUEVA CONEXIÓN A SUPABASE (Usando el Transaction Pooler puerto 6543)!
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres.okydwhigvrzdjfzumvun:Dn35vNv8dJeMi2Qv@aws-1-us-east-2.pooler.supabase.com:6543/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.okydwhigvrzdjfzumvun:SCySBRq9MyceQ03G@aws-1-us-east-2.pooler.supabase.com:5432/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
